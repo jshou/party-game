a59d0dc97636df51bd200db11fbf5f685242bc75
jshou <joshuahou@gmail.com>
2015-01-28 17:46:26 -0800

    Created PartierSpawner prefab

---
+++ app/Assets/Scripts/PartierSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartierSpawner : MonoBehaviour {
+  	public GameObject PartierPrefab;
+  	public int Count;
+
+	void Start() {
+    	
+    	for (int i = 0; i < Count; i++) {
+	      	GameObject.Instantiate(PartierPrefab, new Vector3(i, i, -1), Quaternion.identity);
+    	}
+	}
+}
