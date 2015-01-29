54ac7b3f3e61ee52371ead030e5c203e0a30e7f5
jshou <joshuahou@gmail.com>
2015-01-28 18:01:27 -0800

    Defined Partier bounds

--- app/Assets/Scripts/PartierSpawner.cs
+++ app/Assets/Scripts/PartierSpawner.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class PartierSpawner : MonoBehaviour {
   	public GameObject PartierPrefab;
   	public int Count;
+  	public float XMin;
+  	public float XMax;
+  	public float YMin;
+  	public float YMax;
 
 	void Start() {
-    	
     	for (int i = 0; i < Count; i++) {
-	      	GameObject.Instantiate(PartierPrefab, new Vector3(i, i, -1), Quaternion.identity);
+	      	GameObject.Instantiate(PartierPrefab, new Vector3(Random.Range(XMin, XMax), Random.Range(YMin, YMax), -1), Quaternion.identity);
     	}
 	}
 }
