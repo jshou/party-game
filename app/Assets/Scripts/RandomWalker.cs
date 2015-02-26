015db4e2818fc231f7492c2631a916fba88b1c85
jshou <joshuahou@gmail.com>
2015-02-25 18:02:39 -0800

    partiers spaz out

---
+++ app/Assets/Scripts/RandomWalker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomWalker : MonoBehaviour {
+  public const float DISTANCE = 0.05f;
+
+  void Update() {
+    var x = gameObject.transform.position.x;
+    var y = gameObject.transform.position.y;
+    var newX = x + Random.Range(-DISTANCE, DISTANCE);
+    var newY = y + Random.Range(-DISTANCE, DISTANCE);
+
+    gameObject.transform.position = new Vector3(newX, newY, gameObject.transform.position.z);
+  }
+}
